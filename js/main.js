ee7dc1a94f6dac5a2ed72c04838f6ba6ddf07556
Ed Glazenski <edglazenski@gmail.com>
2015-11-17 16:27:22 -0500

    Controller added and option to specify female or male also added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 (function(){
   var app = angular.module('mad', []);
 
+  app.controller('WordController', function(){
+     this.gender = 'm';
 
+    this.setGender = function(gender) {
+        this.gender = prompt('Is the name male or female? Type m for male and f for female.');
+
+        
+    };
+
+  
+
+  
+
+  });
 
 
 })();
 
+
+
+//Resets input fields
 $(document).ready(function(){
   $('button').on('click', function(){
     $('input').val('');
